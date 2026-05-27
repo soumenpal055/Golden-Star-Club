abb42d861cb06e697465150447e7b93a7dae452d
SOUMEN PAL <palsoumen013@gmail.com>
2026-05-27 20:45:11 +0530

    Updated Railway database connection

--- src/main/java/servlet/DBConnection.java
+++ src/main/java/servlet/DBConnection.java
@@ -5,27 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-   // Localhost muche Railway er internal link aur database name ('railway') dewa holo
-   private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/railway?useUnicode=true&characterEncoding=UTF-8";
-   
-   private static final String USER = "root";
-   
-   // Ekhane 'soumen123' muche tomar Railway theke copy kora boro password ta bosabe
-   private static final String PASSWORD = "ugHKMMYcLVzfyZknSCifkaLyAOgyfIbc";
+    // 1. Niche [HOST] aur [PORT] er jaigay Railway-er thikana boshao
+    private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/railway?useUnicode=true&characterEncoding=UTF-8";
+    
+    private static final String USER = "root";
+    
+    // 2. Niche [PASSWORD] er jaigay Railway-er password boshao (invarted comma "" er bhetore)
+    private static final String PASSWORD = "ugHKMMYcLVzfyZknSCifkaLyAOgyfIbc";
 
-   public DBConnection() {
-   }
+    public DBConnection() {}
 
-   public static Connection getConnection() throws SQLException {
-      // Ekhane r boro link direct na likhe, uporer variable gulo use kora holo jate code clean thake
-      return DriverManager.getConnection(URL, USER, PASSWORD);
-   }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
-   static {
-      try {
-         Class.forName("com.mysql.cj.jdbc.Driver");
-      } catch (ClassNotFoundException var1) {
-         var1.printStackTrace();
-      }
-   }
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException var1) {
+            var1.printStackTrace();
+        }
+    }
 }
