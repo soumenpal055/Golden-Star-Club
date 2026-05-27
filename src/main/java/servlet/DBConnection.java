9a834fd8cea6883a92479fa951e70ab8ac3ffee3
SOUMEN PAL <palsoumen013@gmail.com>
2026-05-27 19:16:08 +0530

    fixed mobile number validation

--- src/main/java/servlet/DBConnection.java
+++ src/main/java/servlet/DBConnection.java
@@ -5,19 +5,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/personalDB?useUnicode=true&characterEncoding=UTF-8";
-    private static final String USER = "root";
-    private static final String PASSWORD = "soumen123";
+   // Localhost muche Railway er internal link aur database name ('railway') dewa holo
+   private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/railway?useUnicode=true&characterEncoding=UTF-8";
+   
+   private static final String USER = "root";
+   
+   // Ekhane 'soumen123' muche tomar Railway theke copy kora boro password ta bosabe
+   private static final String PASSWORD = "ugHKMMYcLVzfyZknSCifkaLyAOgyfIbc";
 
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+   public DBConnection() {
+   }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
+   public static Connection getConnection() throws SQLException {
+      // Ekhane r boro link direct na likhe, uporer variable gulo use kora holo jate code clean thake
+      return DriverManager.getConnection(URL, USER, PASSWORD);
+   }
+
+   static {
+      try {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+      } catch (ClassNotFoundException var1) {
+         var1.printStackTrace();
+      }
+   }
 }
